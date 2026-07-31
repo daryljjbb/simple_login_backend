9953e5111c0e574cca2b14ceb8b5f395205cd951
Daryl Busson <daryljjbb@yahoo.com>
2026-07-31 06:14:57 -0700

    Added at the end of the settings Rest_framework

--- backend/settings.py
+++ backend/settings.py
@@ -140,3 +140,10 @@ SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(minutes=60),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
 }
+
+REST_FRAMEWORK = {
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
+    ),
+}
+
